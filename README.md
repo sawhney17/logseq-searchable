b89ea4381fa42fdcbf316f1d084e3646c5fab7e8
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-04-03 22:52:02 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# logseq-plugin-starter-template
-A quick and easy to use template to get you going from the get go
+# Logaseq OCR Plugin
+
+A quick utility to conduct OCR parsing on images and insert the result as a property. Allows images to be indexable and therefore searchable by logseq similar to evernote. 
+
+## Instructions
+1. Install the plugin from the marketplace
+2. Restart Logseq
+3. Go to plugin settings and select the target language
+    - The target language is important to be accurate in order for accuracy. Use a langauge code from this website. https://tesseract-ocr.github.io/tessdoc/Data-Files#data-files-for-version-400-november-29-2016
+3. Use the slash command `/perform ocr on text`
+
+## Limitations
+1. Works best with images that just include text without diagrams and images
+
---
+++ index.ts
@@ -0,0 +1,45 @@
+import '@logseq/libs';
+import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
+import Tesseract from 'tesseract.js';
+
+let settings: SettingSchemaDesc[] = [{
+  key: "languageCode",
+  type: "string",
+  title: "target Language in language code",
+  description: "Enter the language you are most likely to use with the extension. The language should be in the form of a language code, from this list. https://tesseract-ocr.github.io/tessdoc/Data-Files#data-files-for-version-400-november-29-2016",
+  default: "eng"
+}]
+logseq.useSettingsSchema(settings)
+async function convertImageToText(e) {
+  let path = (await logseq.App.getCurrentGraph()).path
+  console.log(path)
+  let text = (await logseq.Editor.getBlock(e.uuid)).content
+  let formattedText = text.replace(`../assets/`, `${path}/assets/`)
+  console.log(formattedText)
+  let link = formattedText.match(/(?<=!\[.*?\])\((.*?)\)/g)[0]
+  //remove the first and last character from link
+  link = link.substring(1, link.length - 1)
+  logseq.App.showMsg("Processing OCR!")
+  Tesseract.recognize(
+    link,
+    logseq.settings.languageCode,
+    { logger: m => console.log(m) }
+  ).then(({ data: { text } }) => {
+    // remove all line breaks from text
+    text = text.replace(/\n/g, '')
+    logseq.App.showMsg("Results inserted!")
+    logseq.Editor.upsertBlockProperty(e.uuid, "ocr", text)
+  })
+
+}
+
+
+const main = async () => {
+  console.log('ocr plugin loaded');
+  logseq.Editor.registerSlashCommand('Perform OCR on Text', async (e) => {
+    convertImageToText(e);
+  }
+  )
+}
+
+logseq.ready(main).catch(console.error);
